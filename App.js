3157a0f7e15c6f95d618e2978f1ca57c63352eec
gabrielmedina98 <gabriel_r1998@hotmail.com>
2023-05-24 22:04:07 -0300

    aprendendo react

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+
+
+/* function App() {
+  return (
+    <View>
+      <Text>Olá Mundo!!!!</Text>
+      <Text style={{color: 'red', fontSize: 25, margin: 15}}>
+        Meu primeiro App
+      </Text>
+      <Text style={{fontSize: 18, color: 'green'}}> Aprendendo React</Text>
+      <Logo largura={250} altura={250} fulano="Gabriel" />
+    </View>
+  );
+}
+export default App;
+
+function Logo(props) {
+  let img = 'https://sujeitoprogramador.com/reactlogo.png';
+  return (
+    <View>
+      <Image
+        source={{uri: img}}
+        style={{width: props.largura, height: props.altura}}
+      />
+
+      <Text> {props.fulano}</Text>
+    </View>
+  );
+}
+ */
